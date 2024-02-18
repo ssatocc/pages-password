ee95aa696fe16e5e7f55e13a4dbcd894f0f8ab6b
ssatocc <ssatocc@gmail.com>
2024-02-18 13:50:45 +0900

    Create mkdir.js

---
+++ mkdir.js
@@ -0,0 +1,33 @@
+const crypto = require('crypto');
+const fs = require('fs').promises;
+const path = require('path');
+
+async function sha256(pw) {
+    return crypto.createHash('sha256').update(pw, 'utf8').digest('hex');
+}
+
+async function makeHashDirectory(pw) {
+    try {
+        const hash = await sha256(pw);
+        const dirPath = path.join('docs', hash);
+        try {
+            await fs.access(dirPath);
+            console.log(`"${dirPath}" already exists.`);
+        } catch {
+            await fs.mkdir(dirPath, { recursive: true });
+            await fs.writeFile(path.join(dirPath, 'index.html'), 'index\n');
+            console.log(`"${dirPath}" is created.`);
+        }
+    } catch (err) {
+        console.error('Error:', err);
+    }
+}
+
+(async () => {
+    const password = process.argv[2];
+    if (password) {
+        await makeHashDirectory(password);
+    } else {
+        console.error('Error: No password provided.');
+    }
+})();
